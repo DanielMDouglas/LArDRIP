4612215c36f277d32d9c622fca13e4b784284ed5
Daniel Douglas <dougl215@slac.stanford.edu>
2023-08-16 12:07:20 -0700

    added setuptools script

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import setuptools
+
+VER = "0.0.1"
+
+reqs = ["numpy",
+        "scipy",
+        "plotly",
+        "pyaml",
+        "h5py",
+        "larcv",
+        "torch",
+        # "MinkowskiEngine",
+        ]
+
+setuptools.setup(
+    name="LArDRIP",
+    version=VER,
+    author="Daniel D. and others",
+    author_email="dougl215@slac.stanford.edu",
+    description="The LArTPC Dead Region Inference Project",
+    url="https://github.com/DanielMDouglas/LArDRIP",
+    packages=setuptools.find_packages(),
+    install_requires=reqs,
+    classifiers=[
+        "Development Status :: 1 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Physics"
+    ],
+    python_requires='>=3.2',
+)
